1c85d1b377ae50d1c54e5f2d386b50356606e41f
Michael Odeh <michael4dominion2@gmail.com>
2025-02-07 12:19:21 +0100

    first commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,16 +10,16 @@ const config: Config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://alapa-docs.github.io',
+  url: 'https://alapajs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/alapa-docs',
   trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'alapajs', // Usually your GitHub org/user name.
-  projectName: 'alapa-docs.github.io', // Usually your repo name.
+  projectName: 'alapa-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
